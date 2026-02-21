74831b60f9ac1f1f770fa04c118668f3940e52cb
Brahmashnudev Chatterjee <brahmashnudevchatterjee@gmail.com>
2026-02-22 03:41:45 +0530

    user model controller created and api testing is done.

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,17 @@ const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.route');
+
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use('/users', userRoutes);
+connectToDb();
+
+
 
 app.use(cors());
 
